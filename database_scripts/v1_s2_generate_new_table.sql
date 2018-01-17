c5c501da720e79d1afbd7becc66c4e69a7b541dd
Brian Sly <Brian.H.Sly@gmail.com>
2018-01-16 22:17:12 -0700

    Update v1_s2_generate_new_table.sql

--- database_scripts/v1_s2_generate_new_table.sql
+++ database_scripts/v1_s2_generate_new_table.sql
@@ -1,1 +1,20 @@
+CREATE TABLE task_type (
+	id bigint NOT NULL AUTO_INCREMENT,
+	user_id bigint NOT NULL,
+	task_name varchar(20) NOT NULL,
+	task_frequency_id bigint NOT NULL,
+	task_difficulty_id bigint NOT NULL,
+	location_id bigint NOT NULL,
+	PRIMARY KEY (id)
+);
 
+CREATE TABLE goal (
+	id int NOT NULL AUTO_INCREMENT,
+	task_type_id int NOT NULL,
+	task_count int NOT NULL,
+	start_timestamp int NOT NULL,
+	deadline_timestamp NOT NULL,
+  end_timestamp NOT NULL,
+  name NOT NULL,
+	PRIMARY KEY (id)
+  );
